7562040ea00d2f0baa0948c9fe1938f65ba0cbfe
Aurimas Žarskis <a.zarskis@paysera.lt>
2019-12-27 12:10:03 +0200

    Created request class

--- src/main/kotlin/com/paysera/lib/mokejimai/entities/CompanyCreationRequest.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/CompanyCreationRequest.kt
@@ -2,9 +2,22 @@ package com.paysera.lib.mokejimai.entities
 
 import com.google.gson.annotations.SerializedName
 
-data class CompanyCreationRequest (
-    @SerializedName("manager_id") val managerId: Int,
-    @SerializedName("type") val type: String,
-    @SerializedName("company_task") val task: CompanyCreationType.CompanyTask? = null,
-    @SerializedName("company_identifier") val identifier: CompanyCreationType.CompanyIdentifier? = null
-)
+data class CompanyCreationRequest(
+    @SerializedName("manager_id") var managerId: Int,
+    @SerializedName("type") var type: String,
+    @SerializedName("company_identifier") val identifier: CompanyCreationType.CompanyIdentifier? = null,
+    @SerializedName("company_task") val task: CompanyCreationType.CompanyTask? = null
+) {
+
+    constructor(
+        managerId: Int,
+        type: String,
+        identifier: CompanyCreationType.CompanyIdentifier? = null
+    ) : this(managerId, type, identifier, null)
+
+    constructor(
+        managerId: Int,
+        type: String,
+        task: CompanyCreationType.CompanyTask? = null
+    ) : this(managerId, type, null, task)
+}
