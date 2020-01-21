6b8f11b658f23e709298f199dbbfe4121e8fb1e3
luksiv <l.sivickas@gmail.com>
2020-01-21 09:23:12 +0200

    Update to 1.6.0 Common SDK

--- src/main/kotlin/com/paysera/lib/mokejimai/retrofit/APIFactory.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/retrofit/APIFactory.kt
@@ -4,15 +4,18 @@ import com.paysera.lib.common.entities.ApiCredentials
 import com.paysera.lib.common.interfaces.TokenRefresherInterface
 import com.paysera.lib.common.retrofit.BaseApiFactory
 import com.paysera.lib.mokejimai.clients.MokejimaiApiClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class APIFactory(
     userAgent: String?,
     credentials: ApiCredentials,
-    timeout: Long? = null
+    timeout: Long? = null,
+    httpLoggingInterceptorLevel: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
 ) : BaseApiFactory<MokejimaiApiClient>(
     userAgent,
     credentials,
-    timeout
+    timeout,
+    httpLoggingInterceptorLevel
 ) {
 
     override fun createClient(baseUrl: String, tokenRefresher: TokenRefresherInterface?): MokejimaiApiClient {
