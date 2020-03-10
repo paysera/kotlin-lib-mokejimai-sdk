a7eab2e6985ce15783efcfc158f087b66dcc3d54
Aurimas Žarskis <a.zarskis@paysera.lt>
2020-03-10 15:59:17 +0200

    Updated UserAddress.kt entity

--- src/main/kotlin/com/paysera/lib/mokejimai/entities/UserAddress.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/UserAddress.kt
@@ -3,12 +3,12 @@ package com.paysera.lib.mokejimai.entities
 data class UserAddress(
     var type: String,
     var countryCode: String,
-    var countryName: String,
+    var countyName: String?,
     var cityName: String,
     var transliteratedCityName: String?,
     var postalCode: String,
     var legacyAddress: String?,
     var streetName: String,
-    var houseNumber: Int,
-    var apartmentNumber: Int
+    var houseNumber: String,
+    var apartmentNumber: String
 )
