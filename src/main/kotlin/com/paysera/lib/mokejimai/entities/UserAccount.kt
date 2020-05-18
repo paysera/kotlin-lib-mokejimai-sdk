516b3082045a05d26d863d939e3038d295ef9424
Edgar Žigis <tranced.freak@gmail.com>
2020-05-18 11:48:49 +0200

    Add getUserAccounts() API method

---
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/UserAccount.kt
@@ -0,0 +1,9 @@
+package com.paysera.lib.mokejimai.entities
+
+data class UserAccount(
+    val id: Int,
+    val identifier: String,
+    val displayName: String,
+    val type: String,
+    val code: Long?
+)
