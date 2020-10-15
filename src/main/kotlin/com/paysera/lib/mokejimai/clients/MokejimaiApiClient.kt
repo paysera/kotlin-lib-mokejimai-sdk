dca74001414e17bb735e001f33b817cc2e3f41e6
GytisBarakauskas <72445222+GytisBarakauskas@users.noreply.github.com>
2020-10-16 00:48:30 +0300

    Added contact phone/email endpoints

--- src/main/kotlin/com/paysera/lib/mokejimai/clients/MokejimaiApiClient.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/clients/MokejimaiApiClient.kt
@@ -4,10 +4,12 @@ import com.paysera.lib.common.entities.MetadataAwareResponse
 import com.paysera.lib.common.retrofit.ApiRequestManager
 import com.paysera.lib.common.retrofit.BaseApiClient
 import com.paysera.lib.mokejimai.entities.*
+import com.paysera.lib.mokejimai.filters.ContactsFilter
 import com.paysera.lib.mokejimai.filters.IdentityDocumentsFilter
 import com.paysera.lib.mokejimai.filters.ManualTransferConfigurationRequestFilter
 import com.paysera.lib.mokejimai.retrofit.APIClient
 import kotlinx.coroutines.Deferred
+import retrofit2.Response
 
 class MokejimaiApiClient(
     private val apiClient: APIClient,
@@ -67,6 +69,86 @@ class MokejimaiApiClient(
         return apiClient.getAvailableIdentityDocuments(
             country = filter.country,
             limit = filter.limit
+        )
+    }
+
+    fun getContactPhones(
+        userIdentifier: String,
+        contactsFilter: ContactsFilter
+    ): Deferred<MetadataAwareResponse<ContactPhone>> {
+        return apiClient.getContactPhones(
+            userIdentifier,
+            limit = contactsFilter.limit,
+            offset = contactsFilter.offset
+        )
+    }
+
+    fun getContactEmails(
+        userIdentifier: String,
+        contactsFilter: ContactsFilter
+    ): Deferred<MetadataAwareResponse<ContactEmail>> {
+        return apiClient.getContactEmails(
+            userIdentifier,
+            limit = contactsFilter.limit,
+            offset = contactsFilter.offset
+        )
+    }
+
+    fun deleteContactPhone(phoneIdentifier: String): Deferred<Response<Unit>> {
+        return apiClient.deleteContactPhone(phoneIdentifier)
+    }
+
+    fun deleteContactEmail(emailIdentifier: String): Deferred<Response<Unit>> {
+        return apiClient.deleteContactEmail(emailIdentifier)
+    }
+
+    fun addContactPhone(addContactPhoneRequest: AddContactPhoneRequest): Deferred<ContactPhone> {
+        return apiClient.addContactPhone(addContactPhoneRequest)
+    }
+
+    fun addContactEmail(addContactEmailRequest: AddContactEmailRequest): Deferred<ContactEmail> {
+        return apiClient.addContactEmail(addContactEmailRequest)
+    }
+
+    fun setContactPhoneAsMain(phoneIdentifier: String): Deferred<ContactPhone> {
+        return apiClient.setContactPhoneAsMain(phoneIdentifier)
+    }
+
+    fun setContactEmailAsMain(emailIdentifier: String): Deferred<ContactEmail> {
+        return apiClient.setContactEmailAsMain(emailIdentifier)
+    }
+
+    fun setContactPhoneAsMain(token: String, phoneIdentifier: String): Deferred<ContactPhone> {
+        return apiClient.setContactPhoneAsMain(
+            "Bearer $token",
+            phoneIdentifier
+        )
+    }
+
+    fun setContactEmailAsMain(token: String, emailIdentifier: String): Deferred<ContactEmail> {
+        return apiClient.setContactEmailAsMain(
+            "Bearer $token",
+            emailIdentifier
+        )
+    }
+
+    fun confirmContactPhone(
+        phoneIdentifier: String,
+        confirmContactPhoneRequest: ConfirmContactPhoneRequest
+    ): Deferred<ContactPhone> {
+        return apiClient.confirmContactPhone(
+            phoneIdentifier,
+            confirmContactPhoneRequest
+        )
+    }
+
+    fun confirmContactEmail(
+        emailIdentifier: String,
+        confirmContactEmailRequest: ConfirmContactEmailRequest
+    ): Deferred<ContactEmail> {
+        return apiClient.confirmContactEmail(
+            emailIdentifier,
+            confirmContactEmailRequest
         )
     }
 }
---
+++ src/main/kotlin/com/paysera/lib/mokejimai/filters/ContactFilter.kt
@@ -0,0 +1,11 @@
+package com.paysera.lib.mokejimai.filters
+
+import com.paysera.lib.common.entities.BaseFilter
+
+class ContactsFilter(
+    limit: Int? = null,
+    offset: Int? = null
+) : BaseFilter(
+    limit = limit,
+    offset = offset
+)
