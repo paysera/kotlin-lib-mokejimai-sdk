e93e3dd5d9c97b818a70917936c2f7f9bd5f4f49
Gytis Barakauskas <g.barakauskas@paysera.lt>
2020-11-12 18:00:09 +0200

    Renamed field in IdentityDocument

--- src/main/kotlin/com/paysera/lib/mokejimai/entities/IdentityDocument.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/IdentityDocument.kt
@@ -3,6 +3,6 @@ package com.paysera.lib.mokejimai.entities
 data class IdentityDocument (
     val id: Int,
     val type: String,
-    val expiry: String,
+    val dateOfExpiry: String,
     val reviewStatus: String
 )
