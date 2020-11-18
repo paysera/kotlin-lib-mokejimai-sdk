b09309b878c4e5b1f6a25e68b5ad96931174f20d
GytisBarakauskas <72445222+GytisBarakauskas@users.noreply.github.com>
2020-11-18 17:26:36 +0200

    Add GET identity documents API method

--- src/main/kotlin/com/paysera/lib/mokejimai/clients/MokejimaiApiClient.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/clients/MokejimaiApiClient.kt
@@ -1,5 +1,6 @@
 package com.paysera.lib.mokejimai.clients
 
+import com.paysera.lib.common.entities.BaseFilter
 import com.paysera.lib.common.entities.MetadataAwareResponse
 import com.paysera.lib.common.retrofit.ApiRequestManager
 import com.paysera.lib.common.retrofit.BaseApiClient
@@ -149,6 +150,17 @@ class MokejimaiApiClient(
         return apiClient.confirmContactEmail(
             emailIdentifier,
             confirmContactEmailRequest
+        )
+    }
+
+    fun getIdentityDocuments(
+        userId: String,
+        filter: BaseFilter
+    ): Deferred<MetadataAwareResponse<PSIdentityDocument>> {
+        return apiClient.getIdentityDocuments(
+            userId,
+            limit = filter.limit,
+            offset = filter.offset
         )
     }
 }
--- src/main/kotlin/com/paysera/lib/mokejimai/retrofit/APIClient.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/retrofit/APIClient.kt
@@ -133,4 +133,11 @@ interface APIClient {
         @Path("id") id: String,
         @Body confirmContactEmailRequest: ConfirmContactEmailRequest
     ): Deferred<ContactEmail>
+
+    @GET("identity-document/rest/v1/identity-documents")
+    fun getIdentityDocuments(
+        @Query("user_id") userId: String,
+        @Query("limit") limit: Int?,
+        @Query("offset") offset: Int?
+    ): Deferred<MetadataAwareResponse<PSIdentityDocument>>
 }
