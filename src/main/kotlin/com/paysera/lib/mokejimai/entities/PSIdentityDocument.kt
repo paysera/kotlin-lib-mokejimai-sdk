4831274354bac2c93a58fc8139841b08dc82b978
GytisBarakauskas <72445222+GytisBarakauskas@users.noreply.github.com>
2021-01-07 12:12:37 +0200

    Change DateOfExpiry to nullable type (#16)

--- src/main/kotlin/com/paysera/lib/mokejimai/entities/PSIdentityDocument.kt
+++ src/main/kotlin/com/paysera/lib/mokejimai/entities/PSIdentityDocument.kt
@@ -3,6 +3,6 @@ package com.paysera.lib.mokejimai.entities
 data class PSIdentityDocument (
     val id: Int,
     val type: String,
-    val dateOfExpiry: String,
+    val dateOfExpiry: String?,
     val reviewStatus: String
 )
