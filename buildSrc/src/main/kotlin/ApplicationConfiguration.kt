e5cdab9e36455ec9c40d80ef034a193963064e9e
GIlbert Parreño <gilbertparreno95@gmail.com>
2021-05-27 10:44:36 +0800

    migrate to kotlin-dsl and set up for maven central (#23)

---
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -0,0 +1,10 @@
+object ApplicationConfiguration {
+    const val artifactId = "kotlin-lib-mokejimai-sdk"
+    const val groupId = "com.paysera.lib"
+    const val version = "2.15.1-SNAPSHOT"
+    const val name = "Kotlin Library Mokejimai SDK"
+    const val url = "https://www.paysera.com/v2/en-GB/index"
+    const val description = "Paysera: Kotlin Mokejimai SDK Library"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
