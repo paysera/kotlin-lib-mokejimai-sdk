bead67f8a7d200094b0f08caa0265d4388568d72
Stanislav Georgiev <stanislav.georgiev@paysera.net>
2023-08-14 12:48:02 +0300

    APP-5717: [Android] Show correlation_id on screen in case of 5xx status code

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-mokejimai-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "2.15.11"
+    const val version = "2.15.12-SNAPSHOT"
     const val name = "Kotlin Library Mokejimai SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Mokejimai SDK Library"
